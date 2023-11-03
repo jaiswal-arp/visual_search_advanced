b7f620738c476f6506ca9b2f84c2578e84158887
PranithaPoosa <114799068+PranithaPoosa@users.noreply.github.com>
2023-11-03 22:58:13 +0000

    similarity model part 1 - 1 finished

---
+++ streamlit/main.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import os
+import cv2
+import pandas as pd
+import tensorflow as tf
+from sklearn.neighbors import NearestNeighbors
+from sklearn.manifold import TSNE
+from scipy.spatial import distance
+from PIL import Image
+import numpy as np
+
+embedding_model = tf.keras.models.load_model('embedding_model.h5')
+
+tsne_embeddings = np.load('tsne_embeddings.npy')
+
+files = os.listdir('./images_original')
+file_dict = {index: filename for index, filename in enumerate(files)}
+
+dropdown = ['None']
+dropdown = dropdown + list(file_dict.values())
+
+selected_value = st.selectbox("Select an image", dropdown)
+
+if not selected_value == 'None':
+    base_image = selected_value
+
+    base_image_index = -1
+    for i,j in file_dict.items():
+        if j == base_image:
+            base_image_index = i
+
+    n_neighbors = st.number_input("Enter number of similar images to select", min_value = 1, max_value = 5)
+    base_embedding = tsne_embeddings[base_image_index]
+    distances = {i:distance.euclidean(base_embedding, embedding) for i,embedding in enumerate(tsne_embeddings)}
+
+    nearest_neighbor_indices = np.argsort(list(distances.values()))[:n_neighbors + 1]
+
+    nearest_neighbor_distances = []
+
+    for i in nearest_neighbor_indices:
+        nearest_neighbor_distances.append(distances[i])
+
+    image_paths = [f'./images_original/{file_dict[i]}' for i in nearest_neighbor_indices]
+
+    st.write("## Base Image")
+    st.image(image_paths[0], width = 300)
+
+    st.write("## Similar Images")
+    for i in range(1, len(image_paths)):
+        st.image(image_paths[i], width = 300)
+else:
+    uploaded_file = st.file_uploader("Upload an image", type = ["jpg"])
+
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption = "Uploaded image", use_column_width = True)
+
+        image = tf.keras.preprocessing.image.img_to_array(image)
+        
